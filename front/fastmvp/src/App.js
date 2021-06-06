67f12f6881329351ec2413cff41dca259705c930
sihyun98 <sihyun98@cau.ac.kr>
2021-06-07 00:55:28 +0900

    sample template

--- front/fastmvp/src/App.js
+++ front/fastmvp/src/App.js
@@ -1,27 +1,37 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> 2021 CoP fastMVP 메인페이지 입니다. </h1>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+  // console.log("print app");
+  ReactDOM.render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    // document.getElementById('root')
   );
+
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <h1> 2021 CoP fastMVP 메인페이지 입니다. </h1>
+  //       {/* <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a> */}
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
